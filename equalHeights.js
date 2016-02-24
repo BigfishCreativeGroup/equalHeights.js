993342378d7b4bcfcc6448d8b1324d5a45dfabb6
zachakbar <zach@thinkbigfish.com>
2016-02-24 09:05:35 -0700

    Added files via upload

---
+++ equalHeights.js
@@ -0,0 +1,26 @@
+function equalHeight(group,breakpoint) {
+	if($(window).width() >= breakpoint){
+		$(group).css('min-height', 0);
+		var tallest = 0;
+		$(group).each(function() {
+			var thisHeight = $(this).outerHeight();
+			if(thisHeight > tallest) {
+				tallest = thisHeight;
+			}
+		});
+		$(group).css('min-height', tallest);
+	}else{
+		$(group).css('min-height', 0);
+	}
+}
+
+/* Example of how to use this function
+$(document).ready(function() {
+	equalHeight('.group',680);
+	
+	$(window).resize(function(){
+		equalHeight('.group',680);
+	});
+	
+});
+*/
